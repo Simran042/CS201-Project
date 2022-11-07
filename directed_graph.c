d15a881d1742880692e1d4e922765a5f5a34a7e8
Simran042 <115691647+Simran042@users.noreply.github.com>
2022-11-07 17:34:43 +0530

    Update directed_graph.c

--- directed_graph.c
+++ directed_graph.c
@@ -3,17 +3,45 @@
 #include<time.h>
 #include<stdio.h>
 //min capacity
-#define MIN 5
+#define MIN 30
 //max capacity
-#define MAX 20
+#define MAX 60
 int main()
 {
-    int vertex = 9999;
+    int vertex = 1000;
     FILE *filePointer;
     filePointer = fopen("data.txt", "w");
 
     fprintf(filePointer,"%d %d\n", vertex, vertex +1);
     srand(time(0));
+    int count = 0;
+    for(int i = 0; i <vertex; i++)
+    {
+        int capacity;
+        capacity = (rand()%(MAX - MIN)) + MIN;
+        if(i == 0)
+        {
+            count += 2;
+        }
+
+        else if(i<vertex-2)
+        {
+            if(i%2 == 0)
+            {
+                count +=2;
+            }
+            else{
+                ++count;
+            }
+        }
+        if(i == vertex-2)
+        {
+            ++count;
+        }
+    }
+    fclose(filePointer);
+    FILE *filePointer_1 = fopen("data.txt", "w");
+    fprintf(filePointer_1,"%d %d\n", vertex, count);
     for(int i = 0; i <vertex; i++)
     {
         int capacity;
@@ -31,16 +59,20 @@ int main()
             {
                 fprintf(filePointer,"%d %d %d\n", i, i-1, capacity);
                 fprintf(filePointer,"%d %d %d\n",i, i +2, capacity);
+ 
             }
             else{
                 fprintf(filePointer,"%d %d %d\n", i, i+2, capacity);
+
             }
         }
         if(i == vertex-2)
         {
             fprintf(filePointer,"%d %d %d\n",i, i+1, capacity);
+
         }
     }
-
+    fclose(filePointer_1);
+    // printf("%d", count);
 
 }
